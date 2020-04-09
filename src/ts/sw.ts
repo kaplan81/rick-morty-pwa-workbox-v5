398275e0db23a4d6718062b2153ea86f17a030e0
kaplan81 <gesteira.pro@gmail.com>
2020-04-10 01:41:08 +0200

    cache routes

--- src/ts/sw.ts
+++ src/ts/sw.ts
@@ -1,5 +1,33 @@
+import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
 declare var self: WorkerGlobalScope & typeof globalThis;
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  /https:\/\/rickandmortyapi.com\/api\/character(?!\/avatar)/,
+  new NetworkFirst({
+    cacheName: 'rickandmortyapi-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 20,
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /https:\/\/rickandmortyapi\.com\/api\/character\/avatar\/(.+)\.(?:jpeg|jpg)/,
+  new StaleWhileRevalidate({
+    cacheName: 'avatar-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+      }),
+    ],
+  }),
+);
